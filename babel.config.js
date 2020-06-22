2be745d09a100556fda7da4da00008b31b0a73df
rbanianom <banianom.rizky@gmail.com>
2020-06-23 02:18:21 +0700

    solved bug on babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: ['next/babel'],
+  env: {
+    test: {
+      presets: ['@babel/preset-env', '@babel/react'],
+    },
+    development: {
+      presets: ['next/babel'],
+    },
+  },
 };
